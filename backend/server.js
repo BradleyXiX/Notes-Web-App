709108e040ed8880e5cf0c3390c2f19fe8b5ac13
Bradley <157209409+BradleyXiX@users.noreply.github.com>
2025-06-25 16:28:58 +0300

    updated homepage:  added navbar component and fetch notes function, backend: installed cors packed to solve fetch isssue

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from "express"
+import cors from "cors";
+import dotenv from "dotenv"
 import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv"
+
+
 //import ratelimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
@@ -9,16 +12,16 @@ const port = process.env.PORT || 5001
 
 const app = express();
 
-
-
 //middleware
 app.use(express.json())
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true, 
+}))
 //app.use(ratelimiter)
 
 //routes
 app.use("/api/notes", notesRoutes);
-
-
 
 connectDB().then(() => {
     app.listen(port, () => {
